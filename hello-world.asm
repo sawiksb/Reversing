19307ff031b213ed1af0ddde5b8c334edc8f8832
satwik <satwiksb95@gmail.com>
2019-06-22 20:07:50 +0530

    first commit

---
+++ hello-world.asm
@@ -0,0 +1,22 @@
+BITS 32
+
+extern printf
+
+section .rodata
+    hello_world: db "Hello, world!", 10, 0
+
+section .text
+    global main
+
+    main:
+        push ebp
+        mov ebp, esp
+
+        push hello_world
+        call printf
+        add esp, 4
+
+        mov eax, 0
+        mov esp, ebp
+        pop ebp
+        ret
